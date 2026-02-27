49a1bc38ca8c0cac4b1e6c5fec7e11535e8f7528
michele-ukmar <michele.ukmar@gmail.com>
2026-02-27 10:09:22 +0100

    query sistemata

--- analyticalQuery.sql
+++ analyticalQuery.sql
@@ -1,9 +1,6 @@
--- Query per trovare i 15 film più redditizi (maggior profitto netto),
--- mostrando per ciascuno la dimensione del cast, i dati finanziari e il voto medio,
--- ed escludendo i record con budget o incassi pari a zero.
 SELECT 
     f.title AS Titolo_Film,
-    COUNT(c.person_id) AS Dimensione_Cast,
+    COUNT(c.personId) AS Dimensione_Cast,
     f.budget AS Budget_Investito,
     f.revenue AS Incasso_Totale,
     (f.revenue - f.budget) AS Profitto_Netto,
@@ -11,9 +8,9 @@ SELECT
 FROM 
     films f
 JOIN 
-    cast c ON f.filmId = c.filmId
+    "cast" c ON f.filmId = c.filmId
 WHERE 
-    f.budget > 0 AND f.revenue > 0 -- Escludiamo i film con dati finanziari mancanti
+    f.budget > 0 AND f.revenue > 0 
 GROUP BY 
     f.filmId, 
     f.title, 
